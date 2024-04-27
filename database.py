cfd888cccc708cb8a5633a32236b07dbf0c8ace5
al1vel <m1233danil@bk.ru>
2024-04-27 16:00:16 +0300

    added a script to initiate database

---
+++ database.py
@@ -0,0 +1,33 @@
+connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+login TEXT NOT NULL,
+password TEXT NOT NULL,
+pass_level INTEGER,
+email TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Rooms_Information (
+room_id INTEGER PRIMARY KEY,
+room_name TEXT NOT NULL,
+Information TEXT NOT NULL
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS History_of_Operations (   
+operation_id INTEGER PRIMARY KEY,
+room_id INTEGER,
+type_of_operation TEXT NOT NULL,
+booker TEXT NOT NULL,
+date INTEGER,
+time_from INTEGER,
+time_to INTEGER
+)
+''')
--- main.py
+++ main.py
@@ -4,37 +4,6 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-login TEXT NOT NULL,
-password TEXT NOT NULL,
-pass_level INTEGER,
-email TEXT NOT NULL
-)
-''')
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Rooms_Information (
-room_id INTEGER PRIMARY KEY,
-room_name TEXT NOT NULL,
-Information TEXT NOT NULL
-)
-''')
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS History_of_Operations (   
-operation_id INTEGER PRIMARY KEY,
-room_id INTEGER,
-type_of_operation TEXT NOT NULL,
-booker TEXT NOT NULL,
-date INTEGER,
-time_from INTEGER,
-time_to INTEGER
-)
-''')
-
 app = FastAPI()
 
 
@@ -43,7 +12,7 @@ async def root():
     return "POHUI"
 
 
-@app.post("/")
+@app.post("/book/")
 def book(user: str, room_name: str, date: str, time_from: int, time_to: int):
     cursor.execute(f'INSERT INTO History_of_Operations (room_id, type_of_operation, booker, date, time_from, time_to)'
                    f' VALUES ({room_name}, booked, {user}, {date}, {time_from}, {time_to})')
