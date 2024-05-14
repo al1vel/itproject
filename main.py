78059f57f92a3cd744f9d70283b72b28fff3724a
Liza Tkachenko <l.tkachenko4321@gmail.com>
2024-05-14 20:53:06 +0300

    .

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from database import initialize_database
 from fastapi.responses import HTMLResponse
 from fastapi.responses import Response
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 initialize_database()
 connection = sqlite3.connect('my_database.db', check_same_thread=False)
@@ -14,6 +15,7 @@ cursor = connection.cursor()
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 class UserLogin(BaseModel):
